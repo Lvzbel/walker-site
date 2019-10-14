b7ee34d5c8ee7f8561e2265d86398301bb947f51
Lvzbel <rcoto@protonmail.com>
2019-10-13 23:04:06 -0700

    Feat: added comments and theme menu

--- header.php
+++ header.php
@@ -10,3 +10,29 @@
 </head>
 
 <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Ella Walker</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li> -->
+      <?php 
+          wp_nav_menu( 
+            array(
+              'theme_location' => 'navbar',
+              'menu_class' => 'navbar-nav ml-auto',
+              
+            )
+           )
+        ?>
+    </div>
+  </nav>
