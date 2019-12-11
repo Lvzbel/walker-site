d56dd723794db795f9b8566b9bb890bdbaf97377
Lvzbel <rcoto@protonmail.com>
2019-12-10 22:33:56 -0800

    Feat: basic footer complete

--- footer.php
+++ footer.php
@@ -1,9 +1,24 @@
 <div class="footer">
   <div class="container text-light">
     <div class="row">
-      <div class="col-12 col-md-6">Social</div>
       <div class="col-12 col-md-6">
-        <h5>For Business inquiries</h5>
+        <h5 class="footer__header">Fallow me on Social Media</h5>
+        <ul class="footer__social-list">
+          <li><a href="#" target="_blank" class="footer__social"><i class="fa fa-bandcamp" aria-hidden="true"></i>
+            </a></li>
+          <li><a href="https://www.instagram.com/ellawalkermusic_/" target="_blank" class="footer__social"><i
+                class="fa fa-instagram" aria-hidden="true"></i>
+            </a></li>
+          <li><a href="#" target="_blank" class="footer__social"><i class="fa fa-facebook-official"
+                aria-hidden="true"></i>
+              </i>
+            </a></li>
+          <li><a href="#" target="_blank" class="footer__social"><i class="fa fa-twitter" aria-hidden="true"></i>
+            </a></li>
+        </ul>
+      </div>
+      <div class="col-12 col-md-6">
+        <h5 class="footer__header">For Business inquiries</h5>
         <p> <a class="text-light" href="mailto: ellawalkermanager@gmail.com">ellawalkermanager@gmail.com</a></p>
       </div>
     </div>
