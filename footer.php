89a9d6e3bc5ddf024882142c4f93ace6320248f2
Lvzbel <rcoto@protonmail.com>
2019-12-10 22:08:25 -0800

    Feat: footer contact information

--- footer.php
+++ footer.php
@@ -1,51 +1,16 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package understrap
- */
-
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-
-$container = get_theme_mod( 'understrap_container_type' );
-?>
-
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
-
-<div class="wrapper" id="wrapper-footer">
-
-	<div class="<?php echo esc_attr( $container ); ?>">
-
-		<div class="row">
-
-			<div class="col-md-12">
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-						<?php understrap_site_info(); ?>
-
-					</div><!-- .site-info -->
-
-				</footer><!-- #colophon -->
-
-			</div><!--col end -->
-
-		</div><!-- row end -->
-
-	</div><!-- container end -->
-
-</div><!-- wrapper end -->
-
-</div><!-- #page we need this extra closing tag here -->
-
+<div class="footer">
+  <div class="container text-light">
+    <div class="row">
+      <div class="col-12 col-md-6">Social</div>
+      <div class="col-12 col-md-6">
+        <h5>For Business inquiries</h5>
+        <p> <a class="text-light" href="mailto: ellawalkermanager@gmail.com">ellawalkermanager@gmail.com</a></p>
+      </div>
+    </div>
+  </div>
+</div>
 <?php wp_footer(); ?>
 
 </body>
 
 </html>
-
