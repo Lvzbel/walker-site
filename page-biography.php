8c4973fc2088d6ff38267a5c9073706e8bad94f7
Lvzbel <rcoto@protonmail.com>
2020-03-04 22:06:09 -0800

    Feat: basic bio page complete

---
+++ page-biography.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+</div>
+<div class="container bg-secondary">
+  <div class="bio">
+    <h1 class="bio__header display-4 text-light">My Biography</h1>
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <div class="bio__container">
+          <img class="bio__img shadow-lg" src="<?php echo get_theme_file_uri('/src/images/whois.jpg') ?>" alt="">
+        </div>
+      </div>
+      <div class="col-12 col-lg-6 text-light">
+        <div class="bio__container">
+          <p class="bio__text text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro temporibus nesciunt quisquam reiciendis hic
+            culpa, perspiciatis quaerat magnam voluptates quis excepturi unde modi quia, odit delectus nostrum rerum
+            illo?
+            Tempore!
+          </p>
+          <p class="bio__text text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro temporibus nesciunt quisquam reiciendis hic
+            culpa, perspiciatis quaerat magnam voluptates quis excepturi unde modi quia, odit delectus nostrum rerum
+            illo?
+            Tempore!
+          </p>
+          <p class="bio__text text-justify">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro temporibus nesciunt quisquam reiciendis hic
+            culpa, perspiciatis quaerat magnam voluptates quis excepturi unde modi quia, odit delectus nostrum rerum
+            illo?
+            Tempore!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
