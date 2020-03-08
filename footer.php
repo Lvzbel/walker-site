a33fe1e8f4399506988600a3d743c6a196bed870
Lvzbel <rcoto@protonmail.com>
2020-03-07 22:45:12 -0800

    Style: footer socials now used stock white bootstrap color

--- footer.php
+++ footer.php
@@ -4,16 +4,18 @@
       <div class="col-12 col-md-6">
         <h5 class="footer__header">Fallow me on Social Media</h5>
         <ul class="footer__social-list">
-          <li><a href="#" target="_blank" class="footer__social"><i class="fa fa-bandcamp" aria-hidden="true"></i>
+          <li><a href="#" target="_blank" class="footer__social bg-white"><i class="fa fa-bandcamp"
+                aria-hidden="true"></i>
             </a></li>
-          <li><a href="https://www.instagram.com/ellawalkermusic_/" target="_blank" class="footer__social"><i
+          <li><a href="https://www.instagram.com/ellawalkermusic_/" target="_blank" class="footer__social bg-white"><i
                 class="fa fa-instagram" aria-hidden="true"></i>
             </a></li>
-          <li><a href="#" target="_blank" class="footer__social"><i class="fa fa-facebook-official"
+          <li><a href="#" target="_blank" class="footer__social bg-white"><i class="fa fa-facebook-official"
                 aria-hidden="true"></i>
               </i>
             </a></li>
-          <li><a href="#" target="_blank" class="footer__social"><i class="fa fa-twitter" aria-hidden="true"></i>
+          <li><a href="#" target="_blank" class="footer__social bg-white"><i class="fa fa-twitter"
+                aria-hidden="true"></i>
             </a></li>
         </ul>
       </div>
