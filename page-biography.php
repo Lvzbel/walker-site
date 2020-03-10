0e99e9fce1f0bd8630b0a8a546bf369b84896327
Lvzbel <rcoto@protonmail.com>
2020-03-09 23:21:01 -0700

    Feat: ACF implementation in biography page

--- page-biography.php
+++ page-biography.php
@@ -2,34 +2,26 @@
 </div>
 <div class="container bg-secondary">
   <div class="bio">
-    <h1 class="bio__header display-4 text-light">My Biography</h1>
+    <h1 class="bio__header display-4 text-light"><?php the_field('title') ?></h1>
     <div class="bio__content row">
       <div class="col-12 col-lg-6">
         <div class="bio__container">
-          <img class="bio__img mb-4 mb-md-5 mb-lg-0 shadow-lg"
-            src="<?php echo get_theme_file_uri('/src/images/whois.jpg') ?>" alt="">
+          <img class="bio__img mb-4 mb-md-5 mb-lg-0 shadow-lg" src="<?= get_field('image')['url'] ?>" alt="">
         </div>
       </div>
       <div class="col-12 col-lg-6 text-light">
         <div class="bio__container">
+          <?php if( have_rows('description') ): ?>
+
+          <?php while( have_rows('description') ): the_row(); ?>
+
           <p class="bio__text text-justify">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro temporibus nesciunt quisquam reiciendis hic
-            culpa, perspiciatis quaerat magnam voluptates quis excepturi unde modi quia, odit delectus nostrum rerum
-            illo?
-            Tempore!
+            <?php the_sub_field('paragraph') ?>
           </p>
-          <p class="bio__text text-justify">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro temporibus nesciunt quisquam reiciendis hic
-            culpa, perspiciatis quaerat magnam voluptates quis excepturi unde modi quia, odit delectus nostrum rerum
-            illo?
-            Tempore!
-          </p>
-          <p class="bio__text text-justify">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro temporibus nesciunt quisquam reiciendis hic
-            culpa, perspiciatis quaerat magnam voluptates quis excepturi unde modi quia, odit delectus nostrum rerum
-            illo?
-            Tempore!
-          </p>
+
+          <?php endwhile; ?>
+
+          <?php endif; ?>
         </div>
       </div>
     </div>
