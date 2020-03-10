bc85cecce519a44af6f0fcb2691c172ce3a27226
Lvzbel <rcoto@protonmail.com>
2020-03-09 22:58:58 -0700

    Feat: ACF implementation in music page

--- page-music.php
+++ page-music.php
@@ -1,38 +1,47 @@
 <?php get_header(); ?>
 
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-image: url('<?= get_field('header_img')['url'] ?>')">
   <div class="container">
-    <h1 class="display-4">Discography</h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <h1 class="display-4 text-light"><?php the_field('header') ?></h1>
+    <p class="lead text-light"><?php the_field('header_lead') ?></p>
   </div>
 </div>
 
 <div class="container">
-  <div class="disco text-light">
-    <h3 class="display-4 ml-5">My Life</h3>
-    <span class="disco__release lead ml-5">Release date: 2018</span>
-    <div class="row mt-5">
-      <div class="col-12 col-md-5">
-        <img class="disco__img shadow-lg rounded" src="<?php echo get_theme_file_uri('/src/images/album.jpg') ?>"
-          alt="">
-      </div>
-      <div class="col-12 col-md-7">
-        <div class="disco__content mt-5 mt-md-0">
-          <p class="disco__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea dolor dolorem
-            sapiente assumenda suscipit vero,
-            rem voluptatum iure nam, quaerat ipsa recusandae ratione, natus blanditiis voluptates earum distinctio et
-            incidunt?</p>
-          <div class="disco__links">
-            <h4 class="disco__listen">Where to listen:</h4>
-            <ul class="disco__links">
-              <li><a class="text-light" href="#">Amazon</a></li>
-              <li><a class="text-light" href="#">Spotify</a></li>
-            </ul>
+  <?php if( have_rows('discography') ): ?>
+
+  <?php while( have_rows('discography') ): the_row(); ?>
+
+  <div class="row">
+    <div class="disco text-light">
+      <h3 class="display-4 ml-5"><?php the_sub_field('title') ?></h3>
+      <span class="disco__release lead ml-5">Release date: <?php the_sub_field('year') ?></span>
+      <div class="row mt-5">
+        <div class="col-12 col-md-5">
+          <img class="disco__img shadow-lg rounded" src="<?= get_sub_field('album_img')['url'] ?>" alt="">
+        </div>
+        <div class="col-12 col-md-7">
+          <div class="disco__content mt-5 mt-md-0">
+            <p class="disco__description"><?php the_sub_field('description') ?></p>
+            <div class="disco__links">
+              <h4 class="disco__listen">Where to listen:</h4>
+              <ul class="disco__links">
+                <li><a class="text-light" href="<?php the_sub_field('amazon_link') ?>">Amazon</a></li>
+                <li><a class="text-light" href="<?php the_sub_field('spotify_link') ?>">Spotify</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
+      <div class="disco__linebreak">
+
+      </div>
     </div>
   </div>
+
+  <?php endwhile; ?>
+
+  <?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
