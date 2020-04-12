034743277c864c5f250c8b6a694aced662b20703
Lvzbel <rcoto@protonmail.com>
2020-04-11 23:33:27 -0700

    Feat: created single event page

---
+++ single-walker-events.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php echo 'Hello from events page' ?>
+
+<?php get_footer(); ?>
