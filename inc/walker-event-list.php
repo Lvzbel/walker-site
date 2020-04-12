2ef672f1d222e51fef355df98e46f640118497ca
Lvzbel <rcoto@protonmail.com>
2020-04-12 00:52:33 -0700

    Feat: added HTML to event-list function

--- inc/walker-event-list.php
+++ inc/walker-event-list.php
@@ -8,7 +8,18 @@
     $events = new WP_Query($args);
 
     while ($events->have_posts()): $events->the_post(); ?>
-<h3><?php the_title(); ?></h3>
+<div class="events__event">
+  <div class="d-flex align-items-center mb-2">
+    <i class="events__icon fa fa-music" aria-hidden="true"></i>
+    <h5><a href="<?php the_permalink(); ?>" class="text-light"><?php the_title(); ?></a></h5>
+  </div>
+  <div class="ml-5">
+    <div class="events__date">
+      Date: <?= get_field('date') ?>
+    </div>
+    <?php the_excerpt(); ?>
+  </div>
+</div>
 <?php endwhile;
   }
 ?>
