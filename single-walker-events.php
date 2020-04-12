4dcd7a3b96cce3edd6399f9a80a306bb9b3452f9
Lvzbel <rcoto@protonmail.com>
2020-04-12 15:22:37 -0700

    Feat: single event page complete

--- single-walker-events.php
+++ single-walker-events.php
@@ -12,6 +12,21 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<?php echo 'Hello from events page' ?>
-
+<div class="container my-5">
+  <?php the_post_thumbnail( 'large', array('class' => 'd-block mx-auto img-fluid shadow') ); ?>
+  <?php while(have_posts()): the_post(); ?>
+  <h1 class="my-3"><?php the_title(); ?></h1>
+  <div class="text-primary">
+    <strong>
+      <?php the_field('date') ?> starting at <?php the_field('time') ?>
+    </strong>
+  </div>
+  <div class="text-secondary mb-3">
+    <strong>
+      Location: <?php the_field('location') ?>
+    </strong>
+  </div>
+  <?php the_content(); ?>
+  <?php endwhile; ?>
+</div>
 <?php get_footer(); ?>
