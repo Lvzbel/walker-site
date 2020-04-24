4f72274def9270285c941346d50d3f7c70bcedc3
Lvzbel <rcoto@protonmail.com>
2020-04-23 23:39:38 -0700

    Feat: changed archive with blog list layout

--- archive.php
+++ archive.php
@@ -1,73 +1,34 @@
-<?php
-/**
- * The template for displaying archive pages.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package understrap
- */
+<?php get_header(); ?>
 
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+<div class="blog container">
+  <h1 class="text-primary mt-5"><?php the_archive_title() ?></h1>
+  <div class="blog__content row d-flex justify-content-around">
 
-get_header();
+    <?php while(have_posts()): the_post(); ?>
+    <?php
+    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); 
+    ?>
 
-$container = get_theme_mod( 'understrap_container_type' );
-?>
+    <div class="blog__post col-11 col-md-5"
+      style="background-image: linear-gradient( 45deg, rgba(0,0,0, 0.6), rgba(0,0,0, 0.7)), url(<?= $image[0] ?>);">
+      <a class="blog__link" href="<?php the_permalink(); ?>">
+        <h2><?php the_title(); ?></h2>
+      </a>
+      <div class="blog__tags">
+        <?php the_tags() ?>
+      </div>
 
-<div class="wrapper" id="archive-wrapper">
+      <p><?php the_excerpt(); ?></p>
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+    </div>
+    <?php endwhile; ?>
+    <!-- The pagination component -->
 
-		<div class="row">
+  </div>
+  <div class="blog__navigation">
+    <?php understrap_pagination(); ?>
+  </div>
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
-			<main class="site-main" id="main">
-
-				<?php if ( have_posts() ) : ?>
-
-					<header class="page-header">
-						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
-
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-						?>
-
-					<?php endwhile; ?>
-
-				<?php else : ?>
-
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
-
-				<?php endif; ?>
-
-			</main><!-- #main -->
-
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
-
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
-		</div> <!-- .row -->
-
-	</div><!-- #content -->
-
-	</div><!-- #archive-wrapper -->
+</div>
 
 <?php get_footer(); ?>
