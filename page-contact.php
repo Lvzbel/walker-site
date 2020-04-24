0c2820ad932b8270a5721d4ade817ac38a4c08e3
Lvzbel <rcoto@protonmail.com>
2020-04-23 17:50:57 -0700

    Feat: Conctact us page created

---
+++ page-contact.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div class="contact"
+  style="background-image: linear-gradient( 45deg, rgba(0,0,0, 0.4), rgba(0,0,0, 0.4) ), url(<?= get_field('contact_image')['url']; ?>);">
+  <div class="container">
+    <?php while(have_posts()): the_post();?>
+
+    <div class="contact__form row d-flex justify-content-center">
+      <div class="col-11">
+        <?php the_content(); ?>
+      </div>
+    </div>
+
+    <?php endwhile; ?>
+  </div>
+</div>
+<?php get_footer(); ?>
